5106bf4677ebaeac528441de661cee5cb48ddab7
Will Holley <will@willholley.com>
2022-03-15 11:48:01 -0400

    Better CI support

--- packages/contracts/tasks/migrations/00.ts
+++ packages/contracts/tasks/migrations/00.ts
@@ -14,7 +14,10 @@ export default task("migration:00", "Initial deployment").setAction(
     const network = hre.network.name;
     let proxyRegistryAddress: string;
 
-    if (network === "rinkeby") {
+    if (
+      network === "rinkeby" ||
+      network === "localhost" // CI
+    ) {
       proxyRegistryAddress = "0xf57b2c51ded3a29e6891aba85459d600256cf317";
     } else if (network === "mainnet") {
       proxyRegistryAddress = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
