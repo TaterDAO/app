1f729d5b1ee663fabff2e5bf1bb81f1de9fa956a
Will Holley <will@willholley.com>
2022-04-05 12:46:22 -0400

    Adds Explore page

---
+++ packages/web/src/pages/explore.tsx
@@ -0,0 +1,15 @@
+// Types
+import type { NextPage } from "next";
+
+// Components
+import Search from "@components/search";
+
+const Explore: NextPage<{}> = ({}) => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+
+export default Explore;
