c3490e0cd7145c9f8753073cddd4c70aa6a51b70
Will Holley <will@willholley.com>
2022-04-12 14:35:20 -0400

    Adds favicon

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -4,6 +4,7 @@ function Document() {
   return (
     <Html>
       <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
