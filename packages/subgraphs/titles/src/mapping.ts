89266ace5bd5dd26ef0d5f6fc517a44df465553a
Will Holley <will@willholley.com>
2022-05-07 19:51:06 -0400

    Adds mapping

--- packages/subgraphs/titles/src/mapping.ts
+++ packages/subgraphs/titles/src/mapping.ts
@@ -1,66 +1,59 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Address, store } from "@graphprotocol/graph-ts";
 import {
   TitleV1_0,
   Approval,
   ApprovalForAll,
   OwnershipTransferred,
-  Transfer
-} from "../generated/TitleV1_0/TitleV1_0"
-import { ExampleEntity } from "../generated/schema"
+  Transfer as TransferEvent,
+} from "../generated/TitleV1_0/TitleV1_0";
+import { Title, Transfer, Account } from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.proxyRegistryAddress(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
-}
+export function handleApproval(event: Approval): void {}
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: TransferEvent): void {
+  const title = new Title(event.params.tokenId.toString());
+
+  // Mint
+  if (event.params.from === Address.zero()) {
+    title.creator = event.params.to;
+    title.createdAt = event.block.timestamp;
+    title.transfers = [];
+  }
+
+  // Burn
+  if (event.params.to === Address.zero()) {
+    store.remove("Title", title.id);
+    // TODO: Also remove the transfers?
+  }
+
+  const from = new Account(event.params.from);
+  const fromTitles = [] as string[];
+  // Filter
+  for (let i = 0; i < fromTitles.length; i++) {
+    const id = fromTitles[i] as string;
+    if (id !== title.id) fromTitles.push(id);
+  }
+  from.titles = fromTitles;
+  from.save();
+
+  const to = new Account(event.params.to);
+  const toTitles = to.titles || [];
+  toTitles.push(title.id);
+  to.titles = toTitles;
+  to.save();
+
+  const transfer = new Transfer(event.transaction.hash.toHexString());
+  transfer.to = to.id;
+  transfer.from = from.id;
+  transfer.timestamp = event.block.timestamp;
+  transfer.save();
+
+  const titleTransfers = title.transfers;
+  titleTransfers.push(transfer.id);
+  title.transfers = titleTransfers;
+  title.save();
+}
