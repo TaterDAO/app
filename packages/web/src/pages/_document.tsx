991f7f9ded4b740f107c56cf2f78b585108b14b7
Will Holley <will@willholley.com>
2022-05-17 19:51:02 -0400

    Misc: fix FOUC

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -1,21 +1,59 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// Components
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+// Types
+import type { DocumentContext, DocumentInitialProps } from "next/document";
+
+// Libs
+import { ServerStyleSheet } from "styled-components";
+
+class Doc extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        // @ts-ignore
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default Document;
+export default Doc;
