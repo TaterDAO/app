2c021f23a0d4a4cf8615c15992ef48bc32856825
Will Holley <will@willholley.com>
2022-05-24 13:11:43 -0400

    Display IPFS images

--- packages/web/src/services/IPFS.ts
+++ packages/web/src/services/IPFS.ts
@@ -3,9 +3,17 @@ import type { Image } from "@T/Image";
 
 // Libs
 import axios from "axios";
+import { create } from "ipfs-http-client";
 
 const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_INFURA_IPFS_API_ENDPOINT}/api/v0/`,
+  headers: {
+    Authorization: process.env.INFURA_IPFS_JWT as string
+  }
+});
+
+const ipfs = create({
+  url: `${process.env.NEXT_PUBLIC_INFURA_IPFS_API_ENDPOINT}/api/v0/`,
   headers: {
     Authorization: process.env.INFURA_IPFS_JWT as string
   }
@@ -36,4 +44,22 @@ async function uploadImage(
   };
 }
 
-export { uploadImage };
+async function fetchImage(uri: string): Promise<string> {
+  const hash = uri.replace("ipfs://", "");
+
+  return new Promise(async (resolve, reject) => {
+    const { data } = await api.post(
+      "cat",
+      {},
+      { params: { arg: hash }, responseType: "blob" }
+    );
+
+    const reader = new FileReader();
+    reader.readAsDataURL(data);
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+  });
+}
+
+export { uploadImage, fetchImage };
