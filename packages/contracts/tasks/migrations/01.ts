21468712048cce808403708cafb0b0d5a32c64a9
Will Holley <will@willholley.com>
2022-08-05 12:19:34 -0400

    Adds migration:01

--- packages/contracts/tasks/migrations/01.ts
+++ packages/contracts/tasks/migrations/01.ts
@@ -1,6 +1,6 @@
 /**
  * @name tasks/migrations/01.ts
- * @description Upgrades the TitleV1_0 contract.
+ * @description Upgrades the TitleV1_0 contract to TitleV1_1.
  */
 import { task } from "hardhat/config";
 import fs from "fs";
@@ -9,28 +9,21 @@ import { PROXY_INSTANCE_ADDRESS_FILEPATH } from "../../constants";
 export default task("migration:01", "Upgrade")
   .addParam("address", "Proxy Instance address")
   .setAction(async ({ address, ci }, hre, runSuper) => {
-    throw new Error("Placeholder for migration upgrades");
+    console.log("[Migration:01] Upgrading TitleV1_0 to TitleV1_1");
+    let instanceAddress: string;
+    if (ci) {
+      // This implementation is only allowed during testing.
+      if (hre.network.name !== "localhost") throw new Error("Testing only");
+      // Read from the config file created in Migration:00
+      instanceAddress = fs.readFileSync(
+        PROXY_INSTANCE_ADDRESS_FILEPATH,
+        "utf-8"
+      );
+    } else {
+      instanceAddress = address;
+    }
 
-    // console.log("Migration:01");
-    // console.log("Upgrading contract");
-
-    // let instanceAddress: string;
-
-    // if (ci) {
-    //   // This implementation is only allowed during testing.
-    //   if (hre.network.name !== "localhost") throw new Error("Testing only");
-
-    //   // Read from the config file created in Migration:00
-    //   instanceAddress = fs.readFileSync(
-    //     PROXY_INSTANCE_ADDRESS_FILEPATH,
-    //     "utf-8"
-    //   );
-    // } else {
-    //   instanceAddress = address;
-    // }
-
-    // const Factory = await hre.ethers.getContractFactory("TitleV1_0");
-    // await hre.upgrades.upgradeProxy(instanceAddress, Factory);
-
-    // console.log("Migration complete");
+    const Factory = await hre.ethers.getContractFactory("TitleV1_1");
+    await hre.upgrades.upgradeProxy(instanceAddress, Factory);
+    console.log("Migration complete");
   });
