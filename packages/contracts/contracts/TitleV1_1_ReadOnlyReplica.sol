01e359e54c67d921a1adf62081d8503ad5b4cd2b
Will Holley <will@willholley.com>
2022-08-08 11:04:53 -0400

    WIP: Contract initialization

--- packages/contracts/contracts/TitleV1_1_ReadOnlyReplica.sol
+++ packages/contracts/contracts/TitleV1_1_ReadOnlyReplica.sol
@@ -5,4 +5,11 @@ pragma solidity ^0.8.10;
 import {TitleV1_1} from "./TitleV1_1.sol";
 import {ReadOnlyReplica} from "../composer/packages/contracts/src/ReadOnlyReplica.sol";
 
-contract TitleV1_1_ReadOnlyReplica is TitleV1_1, ReadOnlyReplica {}
+// TODO: ReadOnlyReplica needs to be upgradable
+contract TitleV1_1_ReadOnlyReplica is TitleV1_1, ReadOnlyReplica {
+  // constructor(address writer) ReadOnlyReplica(writer) {}
+
+  // function initialize(address proxyRegistryAddress_) public initializer {
+  //   TitleV1_1.initialize(proxyRegistryAddress_);
+  // }
+}
