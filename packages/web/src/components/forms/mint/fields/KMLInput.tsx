55d0f606b0c659380ac79a2a11d49aa06cda4a5a
Will Holley <will@willholley.com>
2022-10-30 14:30:03 -0400

    feat: KML input parses kml file

--- packages/web/src/components/forms/mint/fields/KMLInput.tsx
+++ packages/web/src/components/forms/mint/fields/KMLInput.tsx
@@ -1,19 +1,68 @@
 // Components
-import TextInput from "../../TextInput";
+import BaseFileInput from "@components/forms/BaseFileInput";
 
 // Hooks
 import useMintForm from "../useMintForm";
+import { useRef } from "react";
+
+// libs
+import { kml as makeKML } from "@tmcw/togeojson";
+
+// utils
+import { reduceFeaturesToString } from "@libs/TitleLocation";
+
+const FIELD_ID = "attrKml_";
 
 const KMLInput: React.FC<{}> = ({}) => {
   const form = useMintForm();
+  const el = useRef<HTMLInputElement>(null);
+
+  const handleUpload = async (
+    file: File,
+    setError: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    const reader = new FileReader();
+    reader.onload = (_event) => {
+      try {
+        // Use coordinates to set Location value
+        const xml = _event.target?.result as string;
+        const kml = makeKML(new DOMParser().parseFromString(xml, "text/xml"));
+        const coordinateString = reduceFeaturesToString(
+          kml.features.reduce(
+            (memo, feature) => ({
+              ...memo,
+              //@ts-ignore
+              [feature?.id]: feature.geometry?.coordinates
+            }),
+            {}
+          )
+        );
+        form.setValue("attrLocation_", coordinateString);
+      } catch (error) {
+        setError("There was an error processing your KML");
+      }
+
+      // TODO: Upload file to Pinata
+    };
+    reader.readAsText(file);
+  };
+
+  const handleClear = () => {
+    form.setValue(FIELD_ID, "");
+  };
 
   return (
-    <TextInput
+    <BaseFileInput
       form={form}
-      fieldId="attrKml_"
-      placeholder=""
+      fieldId={FIELD_ID}
       label="KML"
-      description=""
+      description="Optional KML file to parse location from."
+      mimeType=".kml"
+      acceptMultiple={false}
+      handleUpload={handleUpload}
+      handleClear={handleClear}
+      filePreview={null}
+      inputRef={el}
     />
   );
 };
