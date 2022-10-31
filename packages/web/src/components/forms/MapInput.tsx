54f5dfeb717e5f70716aaea2818d64aa931c7e33
Will Holley <will@willholley.com>
2022-10-30 21:14:19 -0400

    refactor: store location in mint form state as GeoJSON

--- packages/web/src/components/forms/MapInput.tsx
+++ packages/web/src/components/forms/MapInput.tsx
@@ -6,8 +6,8 @@ import Map from "@components/Map";
 // Types
 import type { GenericFormState } from "@T/Form";
 import type { DrawEvent } from "@components/Map";
-import type { Polygon, Position } from "geojson";
 import type { Result } from "@mapbox/mapbox-gl-geocoder";
+import type { FeatureCollection, Point } from "geojson";
 
 // Hooks
 import { useState, useEffect, useMemo } from "react";
@@ -18,83 +18,58 @@ const MapInput: React.FC<{
   label: string;
   description: string;
 }> = ({ form, fieldId, label, description }) => {
-  // STATE: UI
-  const hasError = Boolean(form.errors[fieldId]);
-  const [interacted, setInteracted] = useState<boolean>(false);
-
-  // STATE: Polygons
-
-  const [polygons, setPolygons] = useState<{
-    [boundBoxId: string]: Position[][];
-  }>({});
-  const boxCount = Object.keys(polygons).length;
-
-  const mergedCoordinateValue = useMemo(
-    () =>
-      Object.values(polygons)
-        .map((coordinates) => coordinates.toString())
-        .join(";"),
-    [polygons]
+  const [value, setValue] = useState<Point | FeatureCollection | undefined>(
+    undefined
   );
 
-  // STATE: Point
-  const [point, setPoint] = useState<{ lng: number; lat: number } | null>(null);
-  const pointSelected = point != null;
-  const pointString = pointSelected ? `${point.lat}, ${point.lng}` : "";
-
   // EFFECTS
-
-  /**
-   * When point or polygon state is set, set interacted as true.
-   */
-  useEffect(() => {
-    if (boxCount > 0 || pointSelected) setInteracted(true);
-  }, [boxCount, pointSelected]);
 
   /**
    * Update form state when coordinates change.
    */
   useEffect(() => {
-    // Don't prematurely show the error message
-    if (!interacted) return;
-
-    form.setValue(fieldId, mergedCoordinateValue);
-    form.validateField(fieldId, mergedCoordinateValue);
-  }, [interacted, mergedCoordinateValue]);
-
-  useEffect(() => {
-    // Don't prematurely show the error message
-    if (!interacted) return;
-
-    form.setValue(fieldId, pointString);
-    form.validateField(fieldId, pointString);
-  }, [interacted, pointString]);
+    form.setValue(fieldId, value);
+    if (!!value) form.validateField(fieldId, value);
+  }, [value]);
 
   // EVENT HANDLERS
 
   const handleMapDraw = (e: DrawEvent) => {
-    for (const feature of e.features) {
-      const id = feature.id as string;
+    setValue((prevState) => {
+      // Default feature set
+      let features =
+        prevState && prevState.type === "FeatureCollection"
+          ? prevState.features
+          : [];
+      if (e.type == "draw.create") {
+        features = [...features, ...e.features];
+      } else if (e.type == "draw.update") {
+        e.features.forEach((updatedFeature) => {
+          features.forEach((feature, index) => {
+            if (updatedFeature.id == feature.id) {
+              features[index] = updatedFeature;
+            }
+          });
+        });
+      } else if (e.type == "draw.delete") {
+        const filterIds = e.features.map((f) => f.id);
+        features = features.filter(
+          (feature) => !filterIds.includes(feature.id)
+        );
+      }
 
-      setPolygons((prevState) => {
-        if (e.type === "draw.delete") {
-          const newState = { ...prevState };
-          delete newState[id];
-          return newState;
-        } else {
-          return {
-            ...prevState,
-            [id]: (feature.geometry as Polygon).coordinates
-          };
-        }
-      });
-    }
+      return {
+        type: "FeatureCollection",
+        features
+      };
+    });
   };
 
-  const handleGeocoderSelection = (result: Result) => {
-    setPoint({
-      lng: result.geometry.coordinates[1],
-      lat: result.geometry.coordinates[0]
+  const handlePointSelection = (result: Result) => {
+    const [lat, lng] = result.geometry.coordinates;
+    setValue({
+      type: "Point",
+      coordinates: [lng, lat]
     });
   };
 
@@ -110,9 +85,12 @@ const MapInput: React.FC<{
       />
       <Map
         onDraw={handleMapDraw}
-        onGeocoderSelection={handleGeocoderSelection}
+        onGeocoderSelection={handlePointSelection}
+        value={value}
       />
-      {hasError && <ErrorMessage>No location selected</ErrorMessage>}
+      {Boolean(form.errors[fieldId]) && (
+        <ErrorMessage>No location selected</ErrorMessage>
+      )}
     </Row>
   );
 };
--- packages/web/src/types/Form.ts
+++ packages/web/src/types/Form.ts
@@ -1,5 +1,6 @@
 import Joi from "joi";
 import type { Image } from "@T/Image";
+import type { FeatureCollection, Point } from "geojson";
 
 interface Option {
   value: string;
@@ -12,20 +13,64 @@ interface ParentLinkedOption extends Option {
   parent: Option | null;
 }
 
-type FieldStrMap = { [fieldId: string]: string };
+export interface MintFormFields {
+  [fieldId: string]: any;
+  name_: string;
+  description_: string;
+  externalUrl_: string;
+  attrLandClassification_: string;
+  attrBuildingClassification_: string;
+  attrLocation_: FeatureCollection | Point | undefined;
+  attrDeed_: string;
+  attrParcels_: string;
+  attrOwner_: string;
+  attrKml_: string;
+  attrTag_: string;
+}
+
+export interface SerializedMintFormFields {
+  name_: string;
+  description_: string;
+  externalUrl_: string;
+  image_: string;
+  attrLandClassification_: string;
+  attrBuildingClassification_: string;
+  attrLocation_: string;
+  attrDeed_: string;
+  attrParcels_: string;
+  attrOwner_: string;
+  attrKml_: string;
+  attrTag_: string;
+}
+
+type MintFormErrorsByField = {
+  [fieldId: string]: string;
+  name_: string;
+  description_: string;
+  externalUrl_: string;
+  image_: string;
+  attrLandClassification_: string;
+  attrBuildingClassification_: string;
+  attrLocation_: string;
+  attrDeed_: string;
+  attrParcels_: string;
+  attrOwner_: string;
+  attrKml_: string;
+  attrTag_: string;
+};
 
 interface GenericFormState {
   submitting: boolean;
-  values: FieldStrMap;
+  values: MintFormFields;
   images: { [fieldId: string]: Image | null };
   requiredFields: Array<string>;
   validationSchema: Joi.ObjectSchema<any>;
-  setValue: (fieldId: string, value: string) => void;
+  setValue: (fieldId: string, value: any) => void;
   setImage: (fieldId: string, value: Image | null) => void;
   setSubmitting: (bool: boolean) => void;
   submit: () => Promise<void>;
-  errors: FieldStrMap;
-  validateField: (fieldId: string, value?: string) => boolean;
+  errors: MintFormErrorsByField;
+  validateField: (fieldId: string, value?: any) => boolean;
 }
 
-export type { Option, ParentLinkedOption, FieldStrMap, GenericFormState };
+export type { Option, ParentLinkedOption, GenericFormState };
