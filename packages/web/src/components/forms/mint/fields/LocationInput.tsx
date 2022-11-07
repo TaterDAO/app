ca682cc2bc1a9426efea9abda17d6ae3386a3b8e
Will Holley <will@willholley.com>
2022-11-07 11:53:29 -0500

    refactor: Map input uses reducer

--- packages/web/src/components/forms/mint/fields/LocationInput.tsx
+++ packages/web/src/components/forms/mint/fields/LocationInput.tsx
@@ -4,6 +4,10 @@ import MapInput from "@components/forms/MapInput";
 // Hooks
 import useMintForm from "../useMintForm";
 
+// Context
+import { ActionType } from "@contexts/mint/types";
+import type { Location } from "@contexts/mint/types";
+
 const LocationInput: React.FC<{}> = ({}) => {
   const form = useMintForm();
 
@@ -13,6 +17,10 @@ const LocationInput: React.FC<{}> = ({}) => {
       fieldId="attrLocation_"
       label="Location"
       description="Search for an address or select the coordinates of your parcel(s) using the Polygon tool."
+      value={form.state.attrLocation_}
+      onChange={(value: Location) =>
+        form.dispatch({ type: ActionType.SetLocation, value })
+      }
     />
   );
 };
