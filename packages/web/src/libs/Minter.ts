5b3dc2ad46af6cfbc84e96c3961414fa65d96598
Will Holley <will@willholley.com>
2022-11-07 09:58:08 -0500

    feat: mint gas estimation

--- packages/web/src/libs/Minter.ts
+++ packages/web/src/libs/Minter.ts
@@ -42,28 +42,42 @@ class Minter {
 
   /**
    * Calls Mint.
+   * @param values Tater metadata.
+   * @param fromAddress Sender address.
+   * @param estimate Estimate gas instead of submitting the transaction?
    * @returns Transaction Hash once its been submitted to the chain
    */
-  async mint(values: any, fromAddress: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      this._contract.methods
-        .mint(
-          values.name_,
-          values.description_,
-          values.externalUrl_,
-          values.image_,
-          values.attrLandClassification_,
-          values.attrLocation_,
-          values.attrDeed_,
-          values.attrParcels_,
-          values.attrOwner_,
-          values.attrKml_,
-          values.attrTag_,
-          values.attrBuildingClassification_
-        )
-        .send({ from: fromAddress })
-        .on("error", reject)
-        .on("transactionHash", resolve);
+  async mint(
+    values: any,
+    fromAddress: string,
+    estimate: boolean = true
+  ): Promise<string> {
+    const call = this._contract.methods.mint(
+      values.name_,
+      values.description_,
+      values.externalUrl_,
+      values.image_,
+      values.attrLandClassification_,
+      values.attrLocation_,
+      values.attrDeed_,
+      values.attrParcels_,
+      values.attrOwner_,
+      values.attrKml_,
+      values.attrTag_,
+      values.attrBuildingClassification_
+    );
+    const options = { from: fromAddress };
+
+    return new Promise(async (resolve, reject) => {
+      if (estimate) {
+        try {
+          return await call.estimateGas(options);
+        } catch (error) {
+          reject(error);
+        }
+      } else {
+        call.send(options).on("error", reject).on("transactionHash", resolve);
+      }
     });
   }
 }
